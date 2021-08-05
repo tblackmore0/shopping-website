06251db5b0539395ef00976c84769f2eaa791e58
Tom <tblackmore0@gmail.com>
2021-08-05 19:03:14 +0100

    building github page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Home from './Components/Home';
 import {NavBar} from './Components/NavBar';
 import Shop from './Components/Shop';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {HashRouter as Router, Switch, Route } from 'react-router-dom';
 import React, { useState } from 'react';
 import {itemsList} from './Components/Shop'
 import {Checkout} from './Components/Checkout';
